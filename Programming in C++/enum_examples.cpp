1c577715be857f0504c9ad6a5734cdfd2bbdc595
Mihail Hernandez <mihailernandes@gmail.com>
2021-02-05 20:23:20 +0200

    Adding enum examples

---
+++ Programming in C++/enum_examples.cpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include "enum_examples.h"
+
+void enum_examples() {
+
+	std::cout << "The suite is DIAMONDS and has an identifier = " << DIAMONDS << std::endl;
+	std::cout << "The suite is HEARTHS  and has an identifier = " << HEARTHS  << std::endl;
+	std::cout << "The suite is CLUBS    and has an identifier = " << CLUBS    << std::endl;
+	std::cout << "The suite is SPADES   and has an identifier = " << SPADES   << std::endl;
+
+
+	std::cout << "The day of the week is MONDAY    and has an identifier = " << MONDAY    << std::endl;
+	std::cout << "The day of the week is TUESDAY   and has an identifier = " << TUESDAY   << std::endl;
+	std::cout << "The day of the week is WEDNESDAY and has an identifier = " << WEDNESDAY << std::endl;
+	std::cout << "The day of the week is THURSDAY  and has an identifier = " << THURSDAY  << std::endl;
+	std::cout << "The day of the week is FRUDAY    and has an identifier = " << FRIDAY    << std::endl;
+	std::cout << "The day of the week is SATURDAY  and has an identifier = " << SATURDAY  << std::endl;
+	std::cout << "The day of the week is SUNDAY    and has an identifier = " << SUNDAY    << std::endl;
+}
---
+++ Programming in C++/enum_examples.h
@@ -0,0 +1,17 @@
+#pragma once
+
+void enum_examples();
+
+enum Suite {
+	DIAMONDS, HEARTHS, CLUBS, SPADES
+};
+
+enum Week {
+	MONDAY    = 1,
+	TUESDAY   = 2,
+	WEDNESDAY = 4,
+	THURSDAY  = 8,
+	FRIDAY    = 12,
+	SATURDAY  = 14,
+	SUNDAY    = 0
+};
