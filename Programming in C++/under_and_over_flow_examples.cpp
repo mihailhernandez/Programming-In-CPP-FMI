fbc36f06d53224d8c71bbf48b43acba6e13b92b2
Mihail Hernandez <mihailernandes@gmail.com>
2021-02-05 16:31:35 +0200

    Adding under and overflow examples

--- Programming in C++/under_and_over_flow_examples.cpp
+++ Programming in C++/under_and_over_flow_examples.cpp
@@ -5,12 +5,13 @@
 
 void under_and_over_flow_examples() {
 	
+	// Underflow
 	int variable = INT_MIN;
 	std::cout << "Min int     = " << variable << std::endl;
 	variable--;
 	std::cout << "Min int - 1 = " << variable << std::endl;
 	
-	
+	// Overflow
 	variable = INT_MAX;
 	std::cout << "Max int     = " << variable << std::endl;
 	variable++;
