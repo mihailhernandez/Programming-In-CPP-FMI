181d37f2f5997e926d4265e73f76ee64a5b03172
Mihail Hernandez <mihailernandes@gmail.com>
2021-02-05 16:10:29 +0200

    Adding header guard example for in preprocessor examples

--- Programming in C++/preprocessor.h
+++ Programming in C++/preprocessor.h
@@ -2,18 +2,26 @@
 
 #include <iostream>
 
-#define PI 3.14159
-#define MAX(a, b) (a > b ? a : b)
+#define PI 3.14159                        // define a macro for the constant PI
+#define MAX(a, b) (a > b ? a : b)         // define a function macro with two variables 'a' and 'b'
 
-#undef PI
-#undef MAX(a, b)
+#undef PI                                 // undefine the macro for the constant PI
+#undef MAX(a, b)                          // undefine the function macro with two variables 'a' and 'b'
 
-#define TABLE_SIZE 10
+#define TABLE_SIZE 10                     // define a new macro for table size with 10
 
-#if TABLESIZE > 10
-#undef TABLE_SIZE
-#define TABLE_SIZE 10
-#endif
+#if TABLESIZE > 10                        // if the table size macros is larger than 10
+#undef TABLE_SIZE                         // undefine the table mactro
+#define TABLE_SIZE 10                     // define it again with a new constant
+#endif                                    // end the if statement
 
 #define FILE_NAME "preprocessor.h"
 #line 777 FILE_NAME
+
+
+// header guard
+#ifndef SOME_HEADER_GUARD_WITH_UNIQUE_NAME 
+#define SOME_HEADER_GUARD_WITH_UNIQUE_NAME 
+
+#endif
+// end of header guard
