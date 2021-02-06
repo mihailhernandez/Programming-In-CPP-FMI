b77cd54e19162d929676a6d58eaefcc15ca0cf6d
Mihail Hernandez <mihailernandes@gmail.com>
2021-02-06 09:22:57 +0200

    Adding array operations header file

---
+++ Programming in C++/array_operations.h
@@ -0,0 +1,14 @@
+#pragma once
+
+int max_element_of_array(const int*, const int);
+int min_element_of_array(const int*, const int);
+
+int sum_of_element_of_array(const int*, const int);
+int element_at_index(const int*, const int, const int);
+int find_first_index_of_elem(const int*, const int, const int);
+int element_count_in_array(const int*, const int, const int);
+
+void sort_array(int*, const int, const bool);
+void add_number_to_array(int*, const int, const int);
+void print_array(const int*, const int);
+void swap(int&, int&);
